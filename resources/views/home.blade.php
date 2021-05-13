0ec35d5e0ffd9858fca9a26d17bbab52a38e2ac1
Bill Kremer <bill.kremer.dev@gmail.com>
2021-05-12 22:09:16 -0500

    again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,34 @@
         <game-game></game-game>
         
     </div> {{--  container end --}}
-<?php print_r($_SERVER['REMOTE_ADDR']);
- print_r($_SERVER['HTTP_X_FORWARDED_FOR']);
- print_r($_SERVER['HTTP_CLIENT_IP']);
+<?php 
+
+// Function to get the user IP address
+function getUserIP() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+
+
+print_r(getUserIP());
+
  print_r(getenv()); ?>
 @endsection
 
