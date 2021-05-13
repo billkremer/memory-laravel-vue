e1555d4b9871ff970f6d040512dad1abc5fc833f
Bill Kremer <bill.kremer.dev@gmail.com>
2021-05-12 21:30:51 -0500

    missed update for test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
         <game-game></game-game>
         
     </div> {{--  container end --}}
-<?php print_r($_SERVER['REMOTE_ADDR']);
-// print_r(getenv()); ?>
+<?php print_r(' x '.$_SERVER['REMOTE_ADDR']);
+ print_r(' x '.$_SERVER['HTTP_X_FORWARDED_FOR']);
+ print_r(' x '.$_SERVER['HTTP_CLIENT_IP']);
+ print_r(' x '.getenv()); ?>
 @endsection
