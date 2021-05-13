bcced1632cfa1219391a404b3dc407f6b2efdaf2
Bill Kremer <bill.kremer.dev@gmail.com>
2021-05-12 22:11:12 -0500

    again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,19 +13,19 @@
 function getUserIP() {
     $ipaddress = '';
     if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        $ipaddress = 'HTTP_CLIENT_IP'.$_SERVER['HTTP_CLIENT_IP'];
     else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        $ipaddress = 'HTTP_X_FORWARDED_FOR'.$_SERVER['HTTP_X_FORWARDED_FOR'];
     else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        $ipaddress = 'HTTP_X_FORWARDED'.$_SERVER['HTTP_X_FORWARDED'];
     else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        $ipaddress = 'HTTP_X_CLUSTER_CLIENT_IP'.$_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
     else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        $ipaddress = 'HTTP_FORWARDED_FOR'.$_SERVER['HTTP_FORWARDED_FOR'];
     else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        $ipaddress = 'HTTP_FORWARDED'.$_SERVER['HTTP_FORWARDED'];
     else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
+        $ipaddress = 'REMOTE_ADDR'.$_SERVER['REMOTE_ADDR'];
     else
         $ipaddress = 'UNKNOWN';
     return $ipaddress;
