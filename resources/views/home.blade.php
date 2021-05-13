2db58730f6331630caffe63ab274e441e004c417
Bill Kremer <bill.kremer.dev@gmail.com>
2021-05-12 22:47:54 -0500

    again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,33 +9,31 @@
     </div> {{--  container end --}}
 <?php 
 
-// Function to get the user IP address
-function getUserIP() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = 'HTTP_CLIENT_IP'.$_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = 'HTTP_X_FORWARDED_FOR'.$_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = 'HTTP_X_FORWARDED'.$_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-        $ipaddress = 'HTTP_X_CLUSTER_CLIENT_IP'.$_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = 'HTTP_FORWARDED_FOR'.$_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = 'HTTP_FORWARDED'.$_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = 'REMOTE_ADDR'.$_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
+
+        $ipaddress = '';
+        if (!empty(getenv('HTTP_CLIENT_IP')))
+            $ipaddress = 'HTTP_CLIENT_IP'.getenv('HTTP_CLIENT_IP');
+        else if(empty(getenv('HTTP_X_FORWARDED_FOR')))
+            $ipaddress = 'HTTP_X_FORWARDED_FOR'.getenv('HTTP_X_FORWARDED_FOR');
+        else if(empty(getenv('HTTP_X_FORWARDED')))
+            $ipaddress = 'HTTP_X_FORWARDED'.getenv('HTTP_X_FORWARDED');
+        else if(empty(getenv('HTTP_X_CLUSTER_CLIENT_IP')))
+            $ipaddress = 'HTTP_X_CLUSTER_CLIENT_IP'.getenv('HTTP_X_CLUSTER_CLIENT_IP');
+        else if(empty(getenv('HTTP_FORWARDED_FOR')))
+            $ipaddress = 'HTTP_FORWARDED_FOR'.getenv('HTTP_FORWARDED_FOR');
+        else if(empty(getenv('HTTP_FORWARDED')))
+            $ipaddress = 'HTTP_FORWARDED'.getenv('HTTP_FORWARDED');
+        else if(empty(getenv('REMOTE_ADDR')))
+            $ipaddress = 'REMOTE_ADDR'.getenv('REMOTE_ADDR');
+        else
+            $ipaddress = 'UNKNOWN';
 
 
 
-print_r(getUserIP());
 
- print_r(getenv()); ?>
+print_r($ipaddress);
+
+//  print_r(getenv()); ?>
 @endsection
 
 
